d75521fc2fa3d9cc380ee468c381a1b9eeea7e4f
Francisco J. Sañudo <30704939+FranciscoSanudo@users.noreply.github.com>
2019-01-05 19:26:59 -0600

    Add files via upload

---
+++ ext_monitors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# This is a configuration for my self in order to get it done in every Linux Machine I Have
+
+import os
+
+##This section is for brightness adjustments in external monitors
+
+ext_monitor = ''
+while ext_monitor not in ('one', 'two'):
+    print('Please type how many monitors are connected:')
+    ext_monitor = input()
+
+print('Entry: ' + ext_monitor)
+
+print('Brightnes level:')
+bri_monitor = float(input())/10
+bri_exec = bri_monitor
+
+if ext_monitor == 'one':
+    sc1_monitor = "xrandr --output DP2-3 --brightness " + str(bri_monitor)
+    os.system(sc1_monitor)
+else:
+    sc2_monitor = "xrandr --output DP2-2 --brightness " + str(bri_monitor)
+    sc3_monitor = "xrandr --output DP2-3 --brightness " + str(bri_monitor)
+    os.system(sc2_monitor)
+    os.system(sc3_monitor)
+
+print('Is it okay? (y/n)')
+
+bri_loop = input()
+while bri_loop not in ('y'):
+    bri_monitor2 = float(input())/10
+    sc2_monitor = "xrandr --output DP2-2 --brightness " + str(bri_monitor2)
+    sc3_monitor = "xrandr --output DP2-3 --brightness " + str(bri_monitor2)
+    os.system(sc2_monitor)
+    os.system(sc3_monitor)
